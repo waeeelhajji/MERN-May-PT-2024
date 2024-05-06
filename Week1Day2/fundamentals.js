e3868b36755fe59166cf148cb902e3b3a5c55d6f
waeeelhajji <waelhajji.contact@gmail.com>
2024-05-06 21:25:23 +0100

    Week1Day2

---
+++ Week1Day2/fundamentals.js
@@ -0,0 +1,73 @@
+//?--------Scoop
+
+// var name = "bob"
+
+// function SayHi() {
+//     name = "alice"
+//     console.log(name)
+// }
+
+// SayHi()//?
+// console.log(name) //?
+
+
+//?--------Hoisting
+// console.log("What is x?",x)
+const x = 5
+
+
+//primitive Datatypes
+// Number
+// String
+// Boolean
+// Null / undefined
+// x = "aaa"
+// console.log(x)
+
+
+//complex Datatypes
+//Array
+//Objects
+
+// const arr = [11, 22, 33, 44]
+// arr[0] = "bob"
+
+
+
+// console.log(arr)
+
+// const person = {
+//     firstName: 'Bob',
+//     lastName: 'Marley',
+//     email: 'bob@marley.com',
+//     password: 'sekureP@ssw0rd9',
+//     username: 'barley',
+// }
+
+// person = "Another Person"
+// console.log(person)
+
+//?=====Destructuring
+const person = { 
+    firstName: 'Bob', 
+    lastName: 'Marley', 
+    email: 'bob@marley.com', 
+    password: 'sekureP@ssw0rd9', 
+    username: 'barley', 
+    createdAt: 1543945177623
+};
+// const firstName = person.firstName
+// const email = person.email
+// console.log(firstName, email)
+
+const { firstName, password } = person
+console.log(firstName, password)
+
+
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+
+const animalIndexOne = animals[1]
+const animalIndexlast = animals[4]
+
+const [,,LastAnimal,,] = animals
+console.log(LastAnimal)
