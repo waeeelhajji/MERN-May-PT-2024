a98ecbafb719cd223d0bc76f1e2e9a1f7ad3ff3c
waeeelhajji <waelhajji.contact@gmail.com>
2024-06-13 08:02:12 +0100

    Week7Day2

--- Week7Day1/Wael-Hajji-Exam/server/routes/movies.routes.js
+++
@@ -1,9 +0,0 @@
-const MovieController = require('../controllers/movie.controller');
-
-module.exports =( app) => {
-    app.get('/api/movies', MovieController.findAllMovies);
-    app.get('/api/movies/:id',MovieController.findOneSingleMovie);
-    app.patch('/api/movies/:id', MovieController.updateExistingMovie);
-    app.post('/api/movies', MovieController.createNewMovie);
-    app.delete('/api/movies/:id', MovieController.deleteAnExistingMovie);
-}
